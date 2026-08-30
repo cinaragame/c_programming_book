634627464c35c1994f9d27e711cd6b366c4e4d48
Flavia Bellesia <f.bellezia@gmail.com>
2026-08-30 23:08:48 +0100

    started fold.c program, which breaks long lines. yet to start main part

---
+++ ch_01/fold.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+                                                                               #define N_COLUMN 50 /* character limit for breaking column */
+#define TAB_SIZE 8 /* how many blanks make a tab */
+#define MAX_SIZE 500 /* maximum input size */
+                                                                               int getLine(char line[], int size);
+
+/* breaks long input lines into two or more shorter lines after last non-blank
+character that occurs before N_COLUMN
+ * if very lomg lines:
+ * if no blanks or tabs before N_COLUMN
+ */
+int main(void)
+{
+int in_cnt, out_cnt; /* input and output char counting variables */ int in_size; /* actual input size */
+int blank; /* blank before n-th column */
+char input[MAX_SIZE], output[MAX_SIZE]; /* user input line, program output line
+*/
+
+while(in_size = getLine(input, MAX_SIZE)) {
+in_cnt = out_cnt = 0;
+blank = findBlank(input, in_size, N_COLUMN);
+/* if size of input < N_COLUMN */
+if(blank == -2)
+                                                                               /* if no blank before N_COLUMN */
+else if(blank == -1)
+
+/* found blank before N_COLUMN */
+else
+
+}
+
+
+
+}
+
+/* finds first blank in array before n-th column
+ * parameters: array to search, array size, n-th column
+ * return: place of first blank before of n-th column
+ * -1 if no blanks found
+ * -2 if size < column
+ */
+int findBlank(char array[], int size, int column)
+{
+/* guard clause not to access non-string memory */
+if(size < column)
+return size-2;
+
+/* >= or just > ? */
+while(column >= 0)
+{
+if(array[column] == '\n')
+        return column;
+column--;
+}
+                                                                               return -1;
+}
+                                                                               /* gets user input and stores it in array
+ * parameters: array to store input
+ * maximum array size * return: number of chars in array
+ */
+int getLine(char line[], int size)
+{
+int c, i;
+
+i = 0; while(i < size-1 && (c = getchar()) != EOF && c != '\n')
+{
+line[i] = c;
+i++;
+}
+if(c == '\n') {
+line[i] = c;
+i++;
+} line[i] = '\0';
+
+return i;
+}
+
+

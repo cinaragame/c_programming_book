806043906e5f25590a410e683a81d29215178487
Flavia Bellesia <f.bellezia@gmail.com>
2026-09-03 22:46:36 +0100

    program seems worse somehow

--- ch_01/fold.c
+++ ch_01/fold.c
@@ -1,7 +1,7 @@
 /* trailing blanks, tabs ? */
 #include <stdio.h>
 
-#define N_COLUMN 5 /* character limit for breaking column */
+#define N_COLUMN 20 /* character limit for breaking column */
 #define TAB_SIZE 8 /* how many blanks make a tab */
 #define MAX_SIZE 500 /* maximum input size */
 
@@ -15,6 +15,7 @@ character that occurs before N_COLUMN
  */
 int main(void)
 {
+	int i;
 	int in_cnt, out_cnt;
 	int in_size;
 	int column, blank;
@@ -23,58 +24,50 @@ int main(void)
 // !!!!!!!!!!!!!!! have to insert tab size in questions
 	while(in_size = getLine(input, MAX_SIZE))
 	{
+		/* guard clause: excludes final '\n' from input size */
+		if(input[in_size-1] == '\n')
+			in_size = in_size - 1;
+
 		in_cnt = out_cnt = 0;
 		column = N_COLUMN;
-		//while there's room in output array
+
+		/* while there's room in output array */
+		/* && while we haven't finished reading the input array */
 		while(out_cnt < MAX_SIZE-1 && in_cnt < in_size)
 		{
-			blank = findBlank(input, in_size, column);
-			if(blank < out_cnt)
-				blank = column;
-			//copy from out_cnt until blank
-			for( ; out_cnt < blank; in_cnt++, out_cnt++) // find out why in_cnt < in_size broke it
+			/* blank for current line size smaller or equal max column */
+			/* makes filling up until the n-th column with a visible char possible */
+			/* useful for limit case example a 20 char + new line ("This is a short text\n") in max 20 column output */
+			/*if((out_cnt + N_COLUMN) < column)//((in_size - out_cnt) <= N_COLUMN) // what if out_cnt + N_COLUMN < column
+				blank = in_size+1; /* puts blank after last valid char */
+			/* else */
+			/*else
+			{*/
+				blank = findBlank(input, in_size, column);
+				if(blank < out_cnt)
+					blank = column;
+			//}
+			/* copy from out_cnt until blank */
+			for( ; out_cnt < blank; in_cnt++, out_cnt++)
 				output[out_cnt] = input[in_cnt];
 			output[out_cnt] = '\n';
 			out_cnt++;
 			if(input[in_cnt] == ' ')
 				in_cnt++;
-			column = blank + N_COLUMN + 1;
+			column = blank + N_COLUMN;// + 1;
 		}
 		//is this necessary? check GDB
 		output[out_cnt] = '\0';
 		out_cnt++;
-		printf("%s", output);
+
+		/**** OUTPUT ****/
+		/* printing table */
+		for(i = 0; i < N_COLUMN; i++)
+			printf("%d", (i+1)%10);
+		/* printing resulting output */
+		printf("\n%s", output);
 	}
 }
-
-//	int column;
-//	int in_cnt, out_cnt; 
-//	/* input and output char counting variables */
-//	int in_size; /* actual input size */
-//	int blank; /* blank before n-th column */
-//	char input[MAX_SIZE], output[MAX_SIZE]; /* user input line, program output line
-
-//	while(in_size = getLine(input, MAX_SIZE)) 
-//	{
-//		in_cnt = out_cnt = 0;
-//		while(out_cnt < MAX_SIZE-1)
-//		{
-//			column	
-//
-//
-//		}
-//		blank = findBlank(input, in_size, N_COLUMN);
-//		/* if size of input < N_COLUMN */
-//		if(blank == -2)
-//
-//		/* if no blank before N_COLUMN */
-//		else if(blank == -1)
-//
-//		/* found blank before N_COLUMN */
-//		else
-//
-//	}
-//}*/
 
 /* finds first blank in array before n-th column
  * parameters: array to search, array size, n-th column

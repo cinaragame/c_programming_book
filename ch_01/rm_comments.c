b16d70a35b3738dc117affaa67dc5b2a8357d27b
Flavia Bellesia <f.bellezia@gmail.com>
2026-09-09 21:06:42 +0100

    remove comments program simplified and now deals with more than one comment of the same type. still want to refactor it, foi main is too long and doing too much

--- ch_01/rm_comments.c
+++ ch_01/rm_comments.c
@@ -10,10 +10,6 @@
 #define OUT				0	/* identifies that we are outside a comment */
 
 int getInput(char input[], int size);
-void find_comment(char array[], int size, int comment[]);
-
-/* FOR NOW, PROGRAM DOES NOT DEAL WITH MULTIPLE COMMENTS, MAYBE WE DON'T NEED THE 
-	FIND COMMENT FUNCATION AS WE DESIGNED IF */
 
 /* program removes all comments from C program
  * handle quoted strings and character constants properly
@@ -21,7 +17,6 @@ void find_comment(char array[], int size, int comment[]);
  */
 int main(void)
 {
-	int		i;						/* generic iteration counter */
 	int		in_cnt, out_cnt;		/* input and output iteration counters */
 	char	input[MAX_SIZE];		/* input array */
 	char	output[MAX_SIZE];		/* output array */
@@ -31,14 +26,6 @@ int main(void)
 	while(getInput(input, MAX_SIZE))
 	{
 		comment_status = OUT;
-		/* comment array starter */
-		i = 0;
-		while (i < NTYPE_CMNTS)
-		{
-			comment[i] = -1;
-			i++;
-		}
-		find_comment(input, MAX_SIZE, comment);
 
 		/* transcribe text without comments */
 		for(in_cnt = 0, out_cnt = 0; input[in_cnt] != '\0'; ++in_cnt)
@@ -46,15 +33,18 @@ int main(void)
 			/* first check for start of comment */ 
 			if(comment_status == OUT)
 			{
-				if(in_cnt == comment[C_BEGIN])
-					comment_status = IN_C;
-				else if(in_cnt == comment[CPP])
-					comment_status = IN_CPP;
+				if(input[in_cnt] == '/')
+				{
+					if(input[in_cnt+1] == '*')
+						comment_status = IN_C;
+					else if(input[in_cnt+1] == '/')
+						comment_status = IN_CPP;
+				}
 			}
 			/* second: if inside comment, check for getting out */
 			if(comment_status == IN_C)
 			{
-				if(in_cnt == comment[C_END])
+				if(input[in_cnt] == '*' && input[in_cnt+1] == '/')
 				{
 					comment_status = OUT;
 					in_cnt++; /* in order to skip the '/' after the '*' */
@@ -77,38 +67,6 @@ int main(void)
 		output[out_cnt] = '\0';
 		printf("\n\n---- OUTPUT ----\n\n%s\n", output);
 		return 0;
-	}
-}
-
-/* locates start and end of comment
- * parameters: 	array to analyze (line[]), array size (size),
- 				array to store comment location (comment[])
- * return: 		no return, but changes "comment[]"
- * limitations:	function doesn't consider nested comments, returning
- 				the last seen location of comment-signaling characters
- */
-void find_comment(char array[], int size, int comment[])
-{
-	int i;
-
-	i = 0;
-	while(i+1 <= size)
-	{
-		/* COMMENT STARTERS */
-		if(array[i] == '/')
-		{
-			/* if C++ style comment */
-			if(array[i+1] == '/')
-				comment[CPP] = i;	
-			/* C style comment starter */
-			else if(array[i+1] == '*')
-				comment[C_BEGIN] = i;
-		}
-		/* COMMENT FINISHERS */
-		else if(array[i] == '*')
-			if(array[i+1] == '/')
-				comment[C_END] = i;
-		i++;
 	}
 }
 

a213dab6cb0bb06de2b21202cd2170468970671c
Flavia Bellesia <f.bellezia@gmail.com>
2026-09-09 21:53:15 +0100

    remove comments program refactored

--- ch_01/rm_comments.c
+++ ch_01/rm_comments.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-#define MAX_SIZE		100	/* input array maximum char size */
-#define NTYPE_CMNTS 	3	/* number of different comment signal types */
-#define C_BEGIN			0	/* identifies classic C comment starter:	" /(asterisk) " */
-#define C_END			1	/* identifies classic C comment finisher:	" (asterisk)/ " */
-#define CPP				2	/* identifies C++ type comments: from " // " till end-of-line */
+#define MAX_SIZE		1000/* input array maximum char size */
 #define IN_C			1	/* identifies that we are inside C-type comment */
 #define IN_CPP			2	/* identifies that we are inside CPP-type comment */
 #define OUT				0	/* identifies that we are outside a comment */
 
+int defineCommentStatus(char input[], int in_cnt, int current_status);
 int getInput(char input[], int size);
 
 /* program removes all comments from C program
@@ -18,9 +15,9 @@ int getInput(char input[], int size);
 int main(void)
 {
 	int		in_cnt, out_cnt;		/* input and output iteration counters */
+	int		last_status;			/* variable for helping identify comments-ending */
 	char	input[MAX_SIZE];		/* input array */
 	char	output[MAX_SIZE];		/* output array */
-	int		comment[NTYPE_CMNTS];	/* comment placement array */
 	int		comment_status;			/* comment IN-OUT status array */
 
 	while(getInput(input, MAX_SIZE))
@@ -30,44 +27,56 @@ int main(void)
 		/* transcribe text without comments */
 		for(in_cnt = 0, out_cnt = 0; input[in_cnt] != '\0'; ++in_cnt)
 		{
-			/* first check for start of comment */ 
-			if(comment_status == OUT)
+			last_status = comment_status;
+			comment_status = defineCommentStatus(input, in_cnt, comment_status);
+			/* compensations in in_cnt when closing comments */
+			if(last_status == IN_C && comment_status == OUT)
 			{
-				if(input[in_cnt] == '/')
-				{
-					if(input[in_cnt+1] == '*')
-						comment_status = IN_C;
-					else if(input[in_cnt+1] == '/')
-						comment_status = IN_CPP;
-				}
-			}
-			/* second: if inside comment, check for getting out */
-			if(comment_status == IN_C)
-			{
-				if(input[in_cnt] == '*' && input[in_cnt+1] == '/')
-				{
-					comment_status = OUT;
-					in_cnt++; /* in order to skip the '/' after the '*' */
-				}
+				in_cnt++; /* in order to skip the '/' after the '*' */
 				continue;
 			}
-			if(comment_status == IN_CPP)
+			else if(last_status == IN_CPP && comment_status == OUT)
 			{
-				if(input[in_cnt] == '\n')
-				{
-					comment_status = OUT;
-					in_cnt--; /* to compensate and not skip the '\n' */
-				}
+				in_cnt--; /* to compensate and not skip the '\n' */
 				continue;
 			}
 			/* normal case: transcribe from input to output */
-			output[out_cnt] = input[in_cnt];
-			out_cnt++;
+			if(comment_status == OUT)
+			{
+				output[out_cnt] = input[in_cnt];
+				out_cnt++;
+			}
 		}
 		output[out_cnt] = '\0';
 		printf("\n\n---- OUTPUT ----\n\n%s\n", output);
 		return 0;
 	}
+}
+
+/* defines if inside or outside comment depending on current "comment status" and current chars
+ * parameters:		array to analise (input), location of char to analise in array (in_cnt),
+ 					current in/out comment status (current_status)
+ * return (macros): OUT: for when finding end to a comment
+ *					IN_C: when entering C-type comment
+ *					IN_CPP:	when entering CPP-type comment
+ *					current_status: when status was not modified
+ */
+int defineCommentStatus(char input[], int in_cnt, int current_status)
+{
+	/* first check for start of comment */ 
+	if(current_status == OUT && input[in_cnt] == '/')
+	{
+		if(input[in_cnt+1] == '*')
+			return IN_C;
+		else if(input[in_cnt+1] == '/')
+			return IN_CPP;
+	}
+	/* second: if inside comment, check for getting out */
+	if(current_status == IN_C && input[in_cnt] == '*' && input[in_cnt+1] == '/')
+			return OUT;
+	if(current_status == IN_CPP && input[in_cnt] == '\n')
+			return OUT;
+	return current_status;
 }
 
 int getInput(char input[], int size)
